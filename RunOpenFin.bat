faeecce8d2c73d1cf07bb78b873d55674bc253bd
Avi Stramer <astramer@gmail.com>
2019-07-13 17:32:18 -0500

    Update RunOpenFin.bat

--- RunOpenFin.bat
+++ RunOpenFin.bat
@@ -16,7 +16,7 @@ SET debuggingPort=0
     SET debuggingPort=%2
  )
  IF "%opt%" == "--config" (
-    SET startupURL=%2"="%3
+    SET startupURL=%2
  )
 
 
@@ -27,10 +27,8 @@ SET debuggingPort=0
 echo %debuggingPort%
 echo %startupURL%
 
-call TerminateOpenfin.bat
-
 SET openfinLocation=%LocalAppData%\OpenFin
 
-%openfinLocation%\OpenFinRVM.exe --config=%startupURL% --support-email="support@openfin.co" --runtime-arguments="--remote-debugging-port=%debuggingPort%"
+%openfinLocation%\OpenFinRVM.exe --config=%startupURL% --support-email="support@openfin.co" --runtime-arguments="--proxy-server=%testable_proxy% --remote-debugging-port=%debuggingPort%"
 
 ENDLOCAL
