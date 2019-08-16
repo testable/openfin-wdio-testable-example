9e05a0c08460e227fd04094a7caa6cbe2549c6ee
Avi Stramer <astramer@gmail.com>
2019-08-16 11:06:31 -0500

    update linux launch

--- RunOpenFin.sh
+++ RunOpenFin.sh
@@ -2,6 +2,7 @@
 
 DEVTOOLS_PORT=0
 CONFIG=app.json
+PROXY_PAC_URL=0
 echo $@
 for var in "$@"
 do
@@ -16,9 +17,15 @@ do
     CONFIG=${var#*=}
     echo "startup_url==$CONFIG"
   fi
+
+  if [[ $var == --proxy-pac-url* ]] ;
+  then
+    PROXY_PAC_URL=${var#*=}
+    echo "proxy_pac_url==$PROXY_PAC_URL"
+  fi
 done
 
-runtimeArgs="--remote-debugging-port=$DEVTOOLS_PORT --proxy-server=$TESTABLE_PROXY --ignore-certificate-errors --proxy-bypass-list=cdn.openfin.co"
+runtimeArgs="--remote-debugging-port=$DEVTOOLS_PORT --proxy-pac-url=$PROXY_PAC_URL --ignore-certificate-errors --proxy-bypass-list=cdn.openfin.co"
 export runtimeArgs
 
 echo "Running openfin"
