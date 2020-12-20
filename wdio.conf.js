043f4a754966673144368688c520b011f4b0588c
Avi Stramer <astramer@gmail.com>
2020-12-19 19:05:24 -0600

    cleanup

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,31 +1,25 @@
 const spawn = require('child_process').spawn;
+const fs = require('fs');
 
 const isWinOS = process.platform === 'win32';
 const launchTarget = isWinOS ? 'RunOpenFin.bat' : `${process.cwd()}/RunOpenFin.sh`;
 const CONFIG_URL = process.env.CONFIG_URL || (isWinOS ? `${process.cwd()}\\app_sample.json` : `${process.cwd()}/app_sample.json`);
 
-/* 
-  Before OpenFin v13/Chromedriver v76, we set an alternate "Chrome" binary for Chromedriver
-  to use (RunOpenFin.[bat|sh]). This launches OpenFin using the debugging port that Chromedriver 
-  normally passes to Chrome as an argument (--remote-debugging-port).
-
-  From Chromedriver v76 on, it no longer chooses a remote debugging port for Chrome and instead Chrome chooses
-  one itself. So intercepting this argument passed from Chromedriver to Chrome no longer works as a part of launching
-  OpenFin. Instead we launch OpenFin BEFORE our test starts on a preset debugging port and then we pass that to 
-  Chromedriver via the debuggerAddress option. This tells Chromedriver to connect to our already running OpenFin instance.
-
-  When running on Testable, we automatically detect that your application configuration is v13+ and allocate a port
-  for you. This is accessible as the CHROME_PORT environment variable. So you can be confident that if this environment
-  variable exists, you need to launch OpenFin as a before step.
-
-  For testing locally you can set a boolean flag here or potentially read the app config yourself to detect.
-  */
-
-// for local runs, set manually
-const LaunchOpenFinBefore = true;
-// for Testable runs, rely on the auto-detect based on the CHROME_PORT env var existing
-const ShouldLaunchBefore = process.env.CONFIG_URL ? typeof process.env.CHROME_PORT !== 'undefined' : LaunchOpenFinBefore;
+// for Testable runs, rely on the version auto-detect (if CHROME_PORT exists its v13+)
+// for local runs just read it from the app_sample.json file
+const ShouldLaunchBefore = process.env.CONFIG_URL ? 
+  typeof process.env.CHROME_PORT !== 'undefined' : 
+  localShouldLaunchBefore();
 const DefaultDebuggingPort = 12565; // also specified in app_sample.json
+
+function localShouldLaunchBefore() {
+  const version = JSON.parse(fs.readFileSync('app_sample.json')).runtime.version;
+  if (version) {
+    let index = version.indexOf('.');
+    return index === -1 ? true : Number(version.substring(0, index)) >= 13;
+  }
+  return true;
+}
 
 exports.config = {
   specs : [
